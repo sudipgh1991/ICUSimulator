60e07bca227a66aab4aeb2a205169e302b8a1d24
Sudip Ghosh <sudipgh1991@gmail.com>
2020-09-25 09:29:33 +0530

    Initial Commit

---
+++ ICU-Simulator-Backend/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ICU_Simulator_Backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ICU-Simulator/src/app/app.component.ts
@@ -0,0 +1,147 @@
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+declare var $: any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'ICU-Simulator';
+  highcharts = Highcharts;
+  hearRateValue = 85;
+  chartOptions = {
+    chart: {
+      type: 'spline',
+      marginRight: 10,
+      backgroundColor: 'rgb(51,51,52)',
+      events: {
+        load: function () {
+          var series = this.series[0];
+          setInterval(function () {
+            var x = new Date().getTime(),
+              y = Math.floor(Math.random() * (85 - 75 + 1) + 75);
+            series.addPoint([x, y], true, true);
+          }, 1000);
+        },
+      },
+    },
+
+    time: {
+      useUTC: false,
+    },
+
+    title: {
+      text: 'Heart Rate',
+      style: {
+        color: '#E0E0E3',
+        textTransform: 'uppercase',
+        fontSize: '20px',
+      },
+    },
+
+    accessibility: {
+      announceNewData: {
+        enabled: true,
+        minAnnounceInterval: 15000,
+        announcementFormatter: function (allSeries, newSeries, newPoint) {
+          if (newPoint) {
+            return 'New point added. Value: ' + newPoint.y;
+          }
+          return false;
+        },
+      },
+    },
+
+    xAxis: {
+      type: 'datetime',
+      tickPixelInterval: 150,
+      gridLineColor: '#707073',
+      labels: {
+        style: {
+          color: '#E0E0E3',
+        },
+      },
+      lineColor: '#707073',
+      minorGridLineColor: '#505053',
+      tickColor: '#707073',
+      title: {
+        style: {
+          color: '#A0A0A3',
+        },
+      },
+    },
+
+    yAxis: {
+      title: {
+        text: 'Value',
+        style: {
+          color: '#A0A0A3',
+        },
+      },
+      plotLines: [
+        {
+          value: 0,
+          width: 1,
+          color: '#808080',
+        },
+      ],
+      gridLineColor: '#707073',
+      labels: {
+        style: {
+          color: '#E0E0E3',
+        },
+      },
+      lineColor: '#707073',
+      minorGridLineColor: '#505053',
+      tickColor: '#707073',
+      tickWidth: 1,
+    },
+
+    tooltip: {
+      headerFormat: '<b>{series.name}</b><br/>',
+      pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}',
+      backgroundColor: 'rgba(0, 0, 0, 0.85)',
+      style: {
+        color: '#F0F0F0',
+      },
+    },
+
+    legend: {
+      enabled: false,
+    },
+
+    exporting: {
+      enabled: false,
+    },
+
+    series: [
+      {
+        name: 'Heart Rate',
+        data: (function () {
+          var data = [],
+            time = new Date().getTime(),
+            i;
+
+          for (i = -19; i <= 0; i += 1) {
+            data.push({
+              x: time + i * 1000,
+              y: Math.floor(Math.random() * (85 - 75 + 1) + 75),
+            });
+          }
+          return data;
+        })(),
+      },
+    ],
+  };
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      $('.highcharts-credits').hide();
+    }, 20);
+
+    setInterval(() => {
+      this.hearRateValue = Math.floor(Math.random() * (85 - 75 + 1) + 75);
+    }, 5000);
+  }
+}
