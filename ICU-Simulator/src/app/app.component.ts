55cc142828b33b3bbc2156e190c6aaa3d2fe5130
Sudip Ghosh <sudipgh1991@gmail.com>
2020-09-26 09:23:07 +0530

    Increase Frequency

--- ICU-Simulator/src/app/app.component.ts
+++ ICU-Simulator/src/app/app.component.ts
@@ -23,133 +23,10 @@ export class AppComponent implements OnInit {
   awRR = 12;
   Tblood = 99;
 
-  chartOptions = {
-    chart: {
-      type: 'line',
-      marginRight: 10,
-      backgroundColor: 'rgb(51,51,52)',
-      events: {
-        load: function () {
-          var series = this.series[0];
-          setInterval(function () {
-            var x = new Date().getTime(),
-              y = Math.floor(Math.random() * (85 - 75 + 1) + 75);
-            series.addPoint([x, y], true, true);
-          }, 700);
-        },
-      },
-    },
-
-    time: {
-      useUTC: false,
-    },
-
-    title: {
-      text: 'Heart Rate',
-      style: {
-        color: '#E0E0E3',
-        fontSize: '20px',
-      },
-    },
-
-    accessibility: {
-      announceNewData: {
-        enabled: true,
-        minAnnounceInterval: 15000,
-        announcementFormatter: function (allSeries, newSeries, newPoint) {
-          if (newPoint) {
-            return 'New point added. Value: ' + newPoint.y;
-          }
-          return false;
-        },
-      },
-    },
-
-    xAxis: {
-      type: 'datetime',
-      tickPixelInterval: 150,
-      gridLineColor: '#707073',
-      labels: {
-        style: {
-          color: '#E0E0E3',
-        },
-      },
-      lineColor: '#707073',
-      minorGridLineColor: '#505053',
-      tickColor: '#707073',
-      title: {
-        style: {
-          color: '#A0A0A3',
-        },
-      },
-    },
-
-    yAxis: {
-      title: {
-        text: 'Value',
-        style: {
-          color: '#A0A0A3',
-        },
-      },
-      plotLines: [
-        {
-          value: 0,
-          width: 1,
-          color: '#808080',
-        },
-      ],
-      gridLineColor: '#707073',
-      labels: {
-        style: {
-          color: '#E0E0E3',
-        },
-      },
-      lineColor: '#707073',
-      minorGridLineColor: '#505053',
-      tickColor: '#707073',
-      tickWidth: 1,
-    },
-
-    tooltip: {
-      headerFormat: '<b>{series.name}</b><br/>',
-      pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}',
-      backgroundColor: 'rgba(0, 0, 0, 0.85)',
-      style: {
-        color: '#F0F0F0',
-      },
-    },
-
-    legend: {
-      enabled: false,
-    },
-
-    exporting: {
-      enabled: false,
-    },
-
-    series: [
-      {
-        name: 'Heart Rate',
-        data: (function () {
-          var data = [],
-            time = new Date().getTime(),
-            i;
-
-          for (i = -19; i <= 0; i += 1) {
-            data.push({
-              x: time + i * 700,
-              y: Math.floor(Math.random() * (85 - 75 + 1) + 75),
-            });
-          }
-          return data;
-        })(),
-      },
-    ],
-  };
-
   chartOptions_heart = {
     chart: {
       type: 'line',
+      animation: false,
       marginRight: 10,
       backgroundColor: 'rgb(51,51,52)',
       events: {
@@ -157,14 +34,14 @@ export class AppComponent implements OnInit {
           var series = this.series[0];
           var counter = 0;
           var seriesPos = 0;
-          var interval = 1000;
+          var interval = 175;
           var intervalID = 0;
           var times = 1;
           const heartData = [
-            [10, 20, 85, 20, 10],
-            [10, 20, 85, 20, 10],
-            [10, 20, 85, 20, 10],
-            [10, 20, 85, 20, 10],
+            [0, 10, 20, 10, 10, 15, 0, 85],
+            [0, 10, 20, 10, 10, 15, 0, 85],
+            [0, 10, 20, 10, 10, 15, 0, 85],
+            [0, 10, 20, 10, 10, 15, 0, 85],
           ];
 
           var addPoint = function () {
@@ -178,15 +55,15 @@ export class AppComponent implements OnInit {
             if ((times > 30 && times < 80) || (times > 150 && times < 200)) {
               console.log('Hello');
               clearInterval(intervalID);
-              interval = 550;
+              interval = 75;
               runInterval();
             } else {
               clearInterval(intervalID);
-              interval = 1000;
+              interval = 175;
               runInterval();
             }
 
-            if (counter == 5) {
+            if (counter == 8) {
               counter = 0;
               seriesPos++;
             }
@@ -302,7 +179,7 @@ export class AppComponent implements OnInit {
 
           for (i = -70; i <= 0; i += 1) {
             data.push({
-              x: time + i * 550,
+              x: time + i * 350,
               y: [10],
             });
           }
