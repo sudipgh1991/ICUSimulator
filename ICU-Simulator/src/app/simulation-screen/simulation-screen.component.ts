a565742f92d0d9a185dcbcb73d9b0f96140aab2d
Sudip Ghosh <sudipgh1991@gmail.com>
2020-10-09 16:55:38 +0530

    Add BP graph

--- ICU-Simulator/src/app/simulation-screen/simulation-screen.component.ts
+++ ICU-Simulator/src/app/simulation-screen/simulation-screen.component.ts
@@ -11,12 +11,16 @@ export class SimulationScreenComponent implements OnInit {
   title = 'ICU-Simulator';
   highcharts = Highcharts;
   hearRateValue = 85;
-  SpO2 = 95;
+  SpO2 = 98;
   systolic = 120;
   diastolic = 80;
   pulse = 78;
   awRR = 12;
   Tblood = 99;
+  heartFrequency = 20;
+  heartRateData = [0, 10, 20, 10, 10, 15, 0, 85];
+  spO2Data = [99, 55, 25, 5, 0];
+  BPData = [0, 85, 70, 75, 50, 55, 45, 35, 25, 15, 0];
 
   chartOptions_heart = {
     chart: {
@@ -123,79 +127,9 @@ export class SimulationScreenComponent implements OnInit {
           enabled: false,
         },
         animation: false,
-        data: [
-          0,
-          10,
-          20,
-          10,
-          10,
-          15,
-          0,
-          85,
-          0,
-          10,
-          20,
-          10,
-          10,
-          15,
-          0,
-          85,
-          0,
-          10,
-          20,
-          10,
-          10,
-          15,
-          0,
-          85,
-          0,
-          10,
-          20,
-          10,
-          10,
-          15,
-          0,
-          85,
-          0,
-          10,
-          20,
-          10,
-          10,
-          15,
-          0,
-          85,
-          0,
-          10,
-          20,
-          10,
-          10,
-          15,
-          0,
-          85,
-          0,
-          10,
-          20,
-          10,
-          10,
-          15,
-          0,
-          85,
-          0,
-          10,
-          20,
-          10,
-          10,
-          15,
-          0,
-          85,
-          0,
-          10,
-          20,
-          10,
-          10,
-          15,
-          0,
-        ],
+        data: [].concat(
+          ...new Array(this.heartFrequency).fill(this.heartRateData)
+        ),
       },
     ],
   };
@@ -291,73 +225,107 @@ export class SimulationScreenComponent implements OnInit {
         name: 'SpO2',
         color: '#FFFF00',
         animation: false,
-        data: [
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-          99,
-          55,
-          25,
-          5,
-          0,
-        ],
+        data: [0, 0, 0, 0].concat(
+          ...new Array(this.heartFrequency).fill(this.spO2Data)
+        ),
+      },
+    ],
+  };
+
+  chartOptions_BP = {
+    chart: {
+      type: 'spline',
+      marginRight: 10,
+      backgroundColor: 'rgb(51,51,52)',
+      animation: false,
+    },
+
+    time: {
+      useUTC: false,
+    },
+
+    title: {
+      text: 'BP',
+      style: {
+        color: '#E0E0E3',
+        fontSize: '20px',
+      },
+    },
+
+    accessibility: {
+      announceNewData: {
+        enabled: true,
+        minAnnounceInterval: 15000,
+        announcementFormatter: function (allSeries, newSeries, newPoint) {
+          if (newPoint) {
+            return 'New point added. Value: ' + newPoint.y;
+          }
+          return false;
+        },
+      },
+    },
+
+    xAxis: {
+      type: 'datetime',
+      gridLineColor: '#707073',
+      labels: false,
+      lineColor: '#707073',
+      minorGridLineColor: '#505053',
+      tickColor: 'transparent',
+      title: {
+        style: {
+          color: '#A0A0A3',
+        },
+      },
+    },
+
+    yAxis: {
+      title: false,
+      plotLines: [
+        {
+          value: 0,
+          width: 1,
+          color: '#808080',
+        },
+      ],
+      gridLineColor: '#707073',
+      labels: false,
+      lineColor: '#707073',
+      minorGridLineColor: '#505053',
+      tickColor: '#707073',
+      tickWidth: 1,
+      min: 0,
+      max: 100,
+    },
+
+    tooltip: {
+      headerFormat: '<b>{series.name}</b><br/>',
+      pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}',
+      backgroundColor: 'rgba(0, 0, 0, 0.85)',
+      style: {
+        color: '#F0F0F0',
+      },
+    },
+
+    legend: {
+      enabled: false,
+    },
+
+    exporting: {
+      enabled: false,
+    },
+
+    series: [
+      {
+        marker: {
+          enabled: false,
+        },
+        name: 'SpO2',
+        color: 'red',
+        animation: false,
+        data: [0, 0, 0, 0].concat(
+          ...new Array(this.heartFrequency).fill(this.BPData)
+        ),
       },
     ],
   };
@@ -368,8 +336,8 @@ export class SimulationScreenComponent implements OnInit {
     }, 20);
 
     setInterval(() => {
-      this.hearRateValue = Math.floor(Math.random() * (85 - 75 + 1) + 75);
-      this.SpO2 = Math.floor(Math.random() * (91 - 99 + 1) + 91);
+      // this.hearRateValue = Math.floor(Math.random() * (85 - 75 + 1) + 75);
+      // this.SpO2 = Math.floor(Math.random() * (91 - 99 + 1) + 91);
       this.systolic = Math.floor(Math.random() * (120 - 140 + 1) + 120);
       this.diastolic = Math.floor(Math.random() * (80 - 90 + 1) + 80);
       this.pulse = Math.floor(Math.random() * (85 - 75 + 1) + 75);
