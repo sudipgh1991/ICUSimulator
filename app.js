09ad5d7674cec3e531c4f272572f272a93bca26c
Sudip Ghosh <sudipgh1991@gmail.com>
2021-07-04 18:19:59 +0530

    Update

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/ICU-Simulator/dist/ICU-Simulator'));
 
 app.get('/*', function(req, res) {
     res.sendFile(
-        path.join(__dirname + 'index.html')
+        path.join(__dirname + '/ICU-Simulator/dist/ICU-Simulator/index.html')
     );
 });
 
